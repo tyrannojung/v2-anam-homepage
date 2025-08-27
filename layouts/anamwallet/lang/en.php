619ad36f46763581d1077a84c38e1027fa8434b5
Dawoon Jung <tyranno2020@gmail.com>
2025-08-27 14:40:09 +0900

    feat: add multilingual support with Korean and English

---
+++ layouts/anamwallet/lang/en.php
@@ -0,0 +1,58 @@
+<?php
+/**
+ * Anam Wallet Layout - English Language File
+ */
+
+// Navigation
+$lang->nav_home = 'Home';
+$lang->nav_about = 'About';
+$lang->nav_news = 'News';
+$lang->nav_security = 'Security';
+$lang->nav_support = 'Support';
+$lang->nav_contact = 'Contact';
+
+// Main page sections
+$lang->main_title = 'Anam145';
+$lang->main_subtitle = 'New Standard for Digital Asset Management';
+$lang->main_description = 'Safe and Convenient Digital Asset Management Solution';
+
+// Buttons
+$lang->btn_more = 'Learn More';
+$lang->btn_download = 'Download';
+$lang->btn_contact = 'Contact Us';
+$lang->btn_submit = 'Submit';
+$lang->btn_cancel = 'Cancel';
+
+// Footer
+$lang->footer_company = 'Company: Anam145';
+$lang->footer_ceo = 'CEO: Jung Ji-hoon';
+$lang->footer_business_no = 'Business Registration No.';
+$lang->footer_address = 'Address: Room 308, Robot Convergence Building, Korea University, 145 Anam-ro, Seongbuk-gu, Seoul';
+$lang->footer_tel = 'Phone';
+$lang->footer_email = 'Email: tyrannojung@anam145.com';
+$lang->footer_copyright = 'Copyright © Anam145. All rights reserved.';
+
+// About page
+$lang->about_title = 'About Us';
+$lang->about_mission = 'Our Mission';
+$lang->about_vision = 'Our Vision';
+$lang->about_values = 'Core Values';
+
+// Security page
+$lang->security_title = 'Security';
+$lang->security_encryption = 'Encryption';
+$lang->security_protection = 'Asset Protection';
+$lang->security_authentication = 'Authentication';
+
+// Contact page
+$lang->contact_title = 'Contact Us';
+$lang->contact_name = 'Name';
+$lang->contact_email = 'Email';
+$lang->contact_phone = 'Phone';
+$lang->contact_message = 'Message';
+$lang->contact_send = 'Send';
+
+// Busan Wallet page
+$lang->busan_wallet_title = 'Busan Wallet';
+$lang->busan_wallet_description = 'Busan City Blockchain-based Digital Voucher';
+$lang->busan_wallet_features = 'Key Features';
---
+++ layouts/anamwallet/lang/ko.php
@@ -0,0 +1,58 @@
+<?php
+/**
+ * Anam Wallet Layout - Korean Language File
+ */
+
+// Navigation
+$lang->nav_home = '홈';
+$lang->nav_about = '소개';
+$lang->nav_news = '뉴스';
+$lang->nav_security = '보안';
+$lang->nav_support = '고객지원';
+$lang->nav_contact = '문의하기';
+
+// Main page sections
+$lang->main_title = '안암145';
+$lang->main_subtitle = '디지털 자산 관리의 새로운 기준';
+$lang->main_description = '안전하고 편리한 디지털 자산 관리 솔루션';
+
+// Buttons
+$lang->btn_more = '자세히 보기';
+$lang->btn_download = '다운로드';
+$lang->btn_contact = '문의하기';
+$lang->btn_submit = '제출';
+$lang->btn_cancel = '취소';
+
+// Footer
+$lang->footer_company = '회사명: 안암145';
+$lang->footer_ceo = '대표: 정지훈';
+$lang->footer_business_no = '사업자등록번호';
+$lang->footer_address = '주소: 서울특별시 성북구 안암로 145, 고려대학교 로봇융합관 308호';
+$lang->footer_tel = '전화';
+$lang->footer_email = '이메일: tyrannojung@anam145.com';
+$lang->footer_copyright = 'Copyright © 안암145. All rights reserved.';
+
+// About page
+$lang->about_title = '회사 소개';
+$lang->about_mission = '우리의 미션';
+$lang->about_vision = '우리의 비전';
+$lang->about_values = '핵심 가치';
+
+// Security page  
+$lang->security_title = '보안';
+$lang->security_encryption = '암호화';
+$lang->security_protection = '자산 보호';
+$lang->security_authentication = '인증';
+
+// Contact page
+$lang->contact_title = '문의하기';
+$lang->contact_name = '이름';
+$lang->contact_email = '이메일';
+$lang->contact_phone = '전화번호';
+$lang->contact_message = '메시지';
+$lang->contact_send = '보내기';
+
+// Busan Wallet page
+$lang->busan_wallet_title = '부산 월렛';
+$lang->busan_wallet_description = '부산시 블록체인 기반 디지털 바우처';
+$lang->busan_wallet_features = '주요 기능';
